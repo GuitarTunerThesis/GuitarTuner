cd4459f2ad4f9abf747c0608c04848cbf040e01a
Henri Vääräniemi <henri.vaaraniemi@hotmail.fi>
2025-05-26 14:41:11 +0300

    Audio page

--- app/src/main/java/com/example/guitartuner/SecondActivity.kt
+++ app/src/main/java/com/example/guitartuner/SecondActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+//AUDIO PAGE
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
